600f2db63e6bc349f1c0ddd782fc6e858320b461
dkvz <github@dkvz.eu>
2021-11-25 15:58:34 +0100

    Comment

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use input_handlers::bind_kb_events;
 fn main() {
     nwg::init().expect("Failed to init Native Windows GUI");
     let _ui = PShortcutsTray::build_ui(Default::default()).expect("Failed to build UI");
+    // I don't think we can avoid having another thread for the
+    // keyboard events:
     thread::spawn(bind_kb_events);
+    // This will block the main thread:
     nwg::dispatch_thread_events();
 }
