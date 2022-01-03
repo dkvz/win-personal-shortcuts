94ba219143df91ea2a073d7bda809d22f6bcde4b
dkvz <github@dkvz.eu>
2022-01-03 13:56:09 +0100

    Uncomment the windows_subsystem thingy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-//#![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 
 use nwg::NativeUi;
 mod p_shortcuts_tray;
 use p_shortcuts_tray::PShortcutsTray;
-mod input_handlers;
 mod app_config;
+mod input_handlers;
 mod notifications;
 use dotenv::dotenv;
 use eyre::Result;
@@ -14,9 +14,7 @@ fn main() -> Result<()> {
 
     nwg::init().expect("Failed to init Native Windows GUI");
     //let ui_base = PShortcutsTray::new();
-    let _ui = PShortcutsTray::build_ui(Default::default())
-        .expect("Failed to build UI");
-    
+    let _ui = PShortcutsTray::build_ui(Default::default()).expect("Failed to build UI");
     // This will block the main thread:
     nwg::dispatch_thread_events();
 
